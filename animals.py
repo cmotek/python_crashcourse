d56d1b589e504e7581d4f99615d1356205511d66
cmotek <38844285+cmotek@users.noreply.github.com>
2021-05-09 13:08:50 -0700

    05092021update

---
+++ animals.py
@@ -0,0 +1,7 @@
+animals = ['Octopus', 'Squid', 'Cuttlefish', 'Nautilus']
+
+for animal in animals:
+	print(f"A {animal} would make a great pet!")
+
+print("These animals lack bones and also remorse for their enemies.")
+print("Any of these animals would make a great pet!")
---
+++ pizza.py
@@ -0,0 +1,6 @@
+pizzas = ['Supreme', 'Stuffed Crust', 'Buffalo Chicken']
+
+for pizza in pizzas:
+	print(f"I really like {pizza} pizza.")
+
+print("When it comes to pizza, I'm a complete pizza lunatic!")
