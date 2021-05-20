9b21a2a7be8991fa45cd47362158d7d2d6b309a5
cmotek <38844285+cmotek@users.noreply.github.com>
2021-05-20 16:36:42 -0700

    05202021update

---
+++ chapterfour/buffet.py
@@ -0,0 +1,7 @@
+buffet = ("crab", "duckwing", "macaroni and cheese", "frosting", "frenchfries")
+for food in buffet:
+	print(food)
+
+buffet = ("catsandwich", "toast", "macaroni and cheese", "frosting", "frenchfries")
+for food in buffet:
+	print(food)
---
+++ chapterfour/pizzaslices.py
@@ -0,0 +1,11 @@
+pizzas = ['Supreme', 'Stuffed Crust', 'Buffalo Chicken']
+friendpizzas = pizzas[:]
+
+pizzas.append("Eggs Benedict Pizza")
+friendpizzas.append("Eggs Florentine Pizza")
+
+print("My favorite pizzas are:")
+print(pizzas)
+
+print("My friend's favorite pizzas are:")
+print(friendpizzas)
