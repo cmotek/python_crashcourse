6eedc341a4df091a5a12fb77f9c7b9d6e6ddd291
cmotek <38844285+cmotek@users.noreply.github.com>
2021-05-21 10:47:13 -0700

    05212021update

---
+++ conditionaltests.py
@@ -0,0 +1,33 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs car == 'SUBARU'?")
+print(car == 'SUBARU')
+
+print("\nIs car == 'Subaru'?")
+print(car == 'Subaru')
+
+print("\nIs car != 9(as an integer)?")
+print(car != 9)
+
+print("\nHow many subarus are there?")
+subarus = 3
+
+print("\nAre there less than 3 subarus?")
+print(subarus < 3)
+
+print("\nAre there three or more subarus?")
+print(subarus >= 3)
+
+print("\nAre there more than 300 subarus?")
+print(subarus > 330)
+
+print("\nAre there less than 4 subarus and more than 2 subarus?")
+print(subarus < 4 and subarus > 2)
+
+print("\nWell are there 3 subarus?")
+print(subarus == 3)
---
+++ moreconditionaltests.py
@@ -0,0 +1,11 @@
+monsters = ['Vampire', 'Wolfman', 'Frankenstein', 'Halftoad']
+monster = 'Godzilla'
+
+print("\nWait, is the monster you're talking about, Rodan?")
+print(monster == 'Rodan')
+
+print("\nSo we're talking about a different monster than Rodan?")
+print(monster != 'Rodan')
+
+print("\nIs this monster godzilla?")
+print("")
